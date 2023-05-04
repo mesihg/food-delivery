b5473a227943f5a9b18f1c9b426e43ed5cb4f27a
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2023-05-04 16:32:58 +0300

    moved to backend folder

--- restaurant/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RestaurantAPIView, RestaurantDetailAPIView
-
-urlpatterns = [
-    path('restaurants/', RestaurantAPIView.as_view(), name="restaurant-list"),
-    path('restaurants/<uuid:pk>/', RestaurantDetailAPIView.as_view(), name="restaurant-detail"),
-]
